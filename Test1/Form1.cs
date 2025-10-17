3a9c4cd8cd3ca4f05a3bd9c09615bd0922655e9b
Brendon <brendonflores12@gmail.com>
2025-10-17 18:35:17 +0800

    I add msgbox

--- Test1/Form1.cs
+++ Test1/Form1.cs
@@ -9,7 +9,7 @@ namespace Test1
 
         private void Form1_Load(object sender, EventArgs e)
         {
-           
+           MessageBox.Show("Welcome to the Test1 application!");    
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -17,6 +17,30 @@ namespace Test1
             string input = textBox1.Text;
             listBox1.Items.Add(input);
             textBox1.Clear();
+
+            try
+            {
+                if (textBox1.Text == "")
+                {
+                    throw new ArgumentNullException("Type what you want to be listed.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+                return;
+            }
+            try
+            {
+                int number = int.Parse(input);
+                MessageBox.Show($"You entered the number: {number}");
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Invalid input. Please enter a valid integer.");
+                return;
+            }
+
         }
     }
 }
